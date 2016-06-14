d44ac8da5b73b48b3767ff24807e7ab8afe4106f
Pui Wing Tam <puiwing.tam@gmail.com>
2016-06-14 00:42:01 -0400

    Added angular functionality for dashboard

--- talkative-ui/src/app/components/user-prefs.js
+++ talkative-ui/src/app/components/user-prefs.js
@@ -1,9 +1,22 @@
 /* global $ */
 module.exports = class UserPreferencesCtrl {
   constructor($element) {
+    // TODO this will be replaced asynchronously
+    // with the values from the backend as the
+    // component loads
     this.phoneNumber = 1234567890;
+    this.promptInterval = '10';
+    this.newsFreq = 2;
+    this.entertainmentFreq = 4;
+    this.factsFreq = 6;
+    this.romanceFreq = 10;
+
     this.editingPhoneNumber = false;
     this.$element = $element;
+  }
+
+  togglePhoneNumber() {
+    this.editingPhoneNumber = !this.editingPhoneNumber;
   }
 
   // connect foundation to angular component lifecycle
