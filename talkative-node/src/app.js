98d4c315375f59048b4faba3dcf8472efc6748a0
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-14 17:22:41 -0400

    Fixes #26 Create separate users controller

--- talkative-node/src/app.js
+++ talkative-node/src/app.js
@@ -4,48 +4,13 @@ const app = koa();
 const koaPg = require('koa-pg');
 const jsonResp = require('koa-json-response');
 const route = require('koa-route');
+const User = require('./users');
 
 // APP CONFIG
 
 app.use(jsonResp())
-  .use(koaPg('postgres://localhost:5432/talkative'))
+  .use(koaPg(config.DATABASE_URL))
   .listen(8080);
-
-// USER CONTROLLER
-
-var User = {
-  all: function *() {
-    let result = yield this.pg.db.client.query_('SELECT id,phone,frequency,active FROM users');
-    let users = result.rows;
-    console.log('result:', users);
-    return this.jsonResp(200, users);
-  },
-
-  active: function *() {
-    let result = yield this.pg.db.client.query_('SELECT id,phone,frequency,active FROM users WHERE active');
-    let users = result.rows;
-    console.log('result:', users);
-    return this.jsonResp(200, users);
-  },
-
-  inactive: function *() {
-    let result = yield this.pg.db.client.query_('SELECT id,phone,frequency,active FROM users WHERE NOT active');
-    let users = result.rows;
-    console.log('result:', users);
-    return this.jsonResp(200, users);
-  },
-
-  show: function *(id) {
-    let result = yield this.pg.db.client.query_(`SELECT id,phone,frequency,active FROM users WHERE id = ${id}`);
-    if (result.rows.length == 0){
-      return this.jsonResp(404, 'Could not find a user with that id.');
-    } else {
-      let user = result.rows;
-      console.log('result:', user);
-      return this.jsonResp(200, user);
-    }
-  }
-};
 
 /// ROUTES
 
