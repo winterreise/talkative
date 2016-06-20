43e2c7dd07bf72caf8f80402d8ab573d021517d6
Pui Wing Tam <puiwing.tam@gmail.com>
2016-06-20 11:13:22 -0400

    Implemented second call to action and retina hero image

--- talkative-ui/src/app/app.js
+++ talkative-ui/src/app/app.js
@@ -16,4 +16,9 @@ angular.module(MODULE_NAME, [])
 .component('userPrefs', {
   template: require('./components/user-prefs.html'),
   controller: require('./components/user-prefs')
+})
+
+.component('talkativeFooter', {
+  template: require('./components/talkative-footer.html'),
+  transclude: true
 });
