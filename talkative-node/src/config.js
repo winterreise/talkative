bda1e19b89d3ee365dacfdf02a0bf1aa32b01cac
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-20 16:50:25 -0400

    Add new config settings

--- talkative-node/src/config.js
+++ talkative-node/src/config.js
@@ -1,8 +1,10 @@
 // CONFIG FOR BACKEND
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/talkative';
+exports.API_URL = process.env.API_URL || 'http://localhost:8080';
 
 // Output config object in development to help with sanity-checking
 if (exports.NODE_ENV === 'development' || exports.NODE_ENV === 'test') {
-  console.log(exports);
+  console.log(`NODE_ENV === ${exports.NODE_ENV}`);
+  console.log(`EXPORTS ARE: ${exports}`);
 }
