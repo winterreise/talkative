9e1da452703bac8c37d6c54e8e87710fe269f010
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-20 19:36:41 -0400

    Fix var name since JSLint complaining

--- talkative-node/src/auth.js
+++ talkative-node/src/auth.js
@@ -1,38 +1,38 @@
-var passport = require('koa-passport')
+const passport = require('koa-passport');
 
-var user = { id: 1, username: 'test' }
+const testUser = { id: 1, username: 'test' };
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id)
-})
+  done(null, testUser.id);
+});
 
 passport.deserializeUser(function(id, done) {
-  done(null, user)
-})
+  done(null, testUser);
+});
 
-var LocalStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(function(username, password, done) {
   // retrieve user ...
   if (username === 'test' && password === 'test') {
-    done(null, user)
+    done(null, testUser);
   } else {
-    done(null, false)
+    done(null, false);
   }
-}))
+}));
 
-var FacebookStrategy = require('passport-facebook').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
-    clientID: 'your-client-id',
-    clientSecret: 'your-secret',
+    clientID: '600680406767470',
+    clientSecret: 'ab2b159cf0a51919ed9406a753330857',
     callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/facebook/callback'
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
-    done(null, user)
+    done(null, testUser);
   }
-))
+));
 
-var TwitterStrategy = require('passport-twitter').Strategy
+const TwitterStrategy = require('passport-twitter').Strategy;
 passport.use(new TwitterStrategy({
     consumerKey: 'HYQU2eZkD89TbS8qPtlXM6tZM',
     consumerSecret: 'DXO12sJBExqG69WYi3G9dC201gyMOXDcf7rIIENygGB90G9eEX',
@@ -40,18 +40,18 @@ passport.use(new TwitterStrategy({
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
-    done(null, user)
+    done(null, testUser);
   }
-))
+));
 
-var GoogleStrategy = require('passport-google-auth').Strategy
+const GoogleStrategy = require('passport-google-auth').Strategy;
 passport.use(new GoogleStrategy({
-    clientId: 'your-client-id',
-    clientSecret: 'your-secret',
+    clientId: '153427664156-2feen2b5tdu0hkcea6jlsb0s0h6listk.apps.googleusercontent.com',
+    clientSecret: 'bXEp_2YxBTCQr4WMOdtIh0xt',
     callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/google/callback'
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
-    done(null, user)
+    done(null, testUser);
   }
-))
+));
