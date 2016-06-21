24da677128f9ff37784eacbc4a04049444dc737b
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-21 12:07:22 -0400

    Fixes #91 Fix config default ports

--- talkative-node/src/auth.js
+++ talkative-node/src/auth.js
@@ -3,7 +3,7 @@
 const passport = require('koa-passport');
 
 passport.serializeUser(function(user, done) {
-  done(null, testUser.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
@@ -14,7 +14,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
     clientID: '600680406767470',
     clientSecret: 'ab2b159cf0a51919ed9406a753330857',
-    callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/facebook/callback'
+    callbackURL: 'http://localhost:' + (process.env.PORT || 8080) + '/auth/facebook/callback'
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
@@ -26,7 +26,7 @@ const TwitterStrategy = require('passport-twitter').Strategy;
 passport.use(new TwitterStrategy({
     consumerKey: 'HYQU2eZkD89TbS8qPtlXM6tZM',
     consumerSecret: 'DXO12sJBExqG69WYi3G9dC201gyMOXDcf7rIIENygGB90G9eEX',
-    callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/twitter/callback'
+    callbackURL: 'http://localhost:' + (process.env.PORT || 8080) + '/auth/twitter/callback'
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
@@ -38,7 +38,7 @@ const GoogleStrategy = require('passport-google-auth').Strategy;
 passport.use(new GoogleStrategy({
     clientId: '153427664156-2feen2b5tdu0hkcea6jlsb0s0h6listk.apps.googleusercontent.com',
     clientSecret: 'bXEp_2YxBTCQr4WMOdtIh0xt',
-    callbackURL: 'http://localhost:' + (process.env.PORT || 3000) + '/auth/google/callback'
+    callbackURL: 'http://localhost:' + (process.env.PORT || 8080) + '/auth/google/callback'
   },
   function(token, tokenSecret, profile, done) {
     // retrieve user ...
